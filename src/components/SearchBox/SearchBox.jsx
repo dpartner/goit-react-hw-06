9b9e009a4db1763818a0b143dae599fe0cef192f
dpartner <d.partner.ua@gmail.com>
2024-10-08 09:44:23 +0300

    add useselector to search component

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,9 +1,11 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import s from "./SearchBox.module.css";
 import { changeFilter } from "../../redux/filtersSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
 const SearchBox = () => {
   const dispatch = useDispatch();
+  const searchStr = useSelector(selectNameFilter);
 
   const handleSearch = (e) => {
     dispatch(changeFilter(e.target.value));
@@ -11,7 +13,12 @@ const SearchBox = () => {
 
   return (
     <div className={s.searchWrap}>
-      <input className={s.input} type="text" onChange={handleSearch} />
+      <input
+        className={s.input}
+        type="text"
+        value={searchStr}
+        onChange={handleSearch}
+      />
       <p className={s.inputName}>Search contact:</p>
     </div>
   );
